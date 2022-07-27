1c6d338b73497cf40377844d0529127e2d729769
Alejandro Ayala <alejandro.ayala@coppel.com>
2022-07-26 20:43:04 -0600

    actualizacion 26 de julio

---
+++ src/js/login.js
@@ -0,0 +1,90 @@
+var cuentas = [
+  { id:1,nombre: 'Mali', clave: 'MalimarAu', saldo: 200 },
+  { id:2,nombre: 'Gera', clave: 'GerardoOrtiz', saldo: 290 },
+  { id:3,nombre: 'Maui', clave: 'Mauiwi', saldo: 67 }
+];
+
+var jsonUsers = JSON.stringify(cuentas)
+localStorage.setItem('cuentas',jsonUsers  );
+
+const usuario = document.getElementById('usuario');
+const password = document.getElementById('password');
+const btnIniciar = document.getElementById('btnIniciar');
+const bienvenido = document.getElementById('bienvenido');
+const logo = document.getElementById('logo');
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+
+var usuarioCuenta = '';
+var userSelected = '';
+
+logo.addEventListener('click', (e) => {
+  window.location="../../index.html"; 
+});
+
+if(usuario)
+{
+  usuario.addEventListener('change',(e) => {
+      userSelected = usuario.selectedIndex;
+      let nombre = usuario.options[userSelected].text; 
+      bienvenido.innerHTML = `Bienvenido ${nombre}`;
+    });
+}
+
+if(btnIniciar){
+  btnIniciar.addEventListener('click', (e) => {
+    validarContra();
+  });
+}
+if(password){
+  password.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      validarContra();
+    }
+  });
+}
+
+if(consulta){
+  consulta.addEventListener('click', (e) => {
+    console.log(localStorage.getItem('nombre'));
+    bienvenido.innerHTML = `Bienvenido ${localStorage.getItem('nombre')}`;
+  });
+}
+
+function validarContra(){
+  var allUsers = localStorage.getItem('cuentas');
+  var arrUsers =  JSON.parse(allUsers);
+  arrUsers.forEach(element => {
+    console.log("elemento: "+element.id);
+    if(element.id == userSelected){
+      if(password.value == element.clave){
+        usuarioCuenta = element.nombre;
+        console.log(usuarioCuenta);
+       localStorage.setItem('nombre',usuarioCuenta);
+       localStorage.setItem('saldo',element.saldo);
+       localStorage.setItem('id',element.id);
+
+        window.location="../../cajero.html"; 
+        
+        // console.log(localStorage.getItem('nombre'));
+        // bienvenido.innerHTML = `Bienvenidos ${localStorage.getItem('nombre')}`;
+      }
+      else{
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+          `<div class="alert alert-danger alert-dismissible" role="alert">`,
+          `   <div>contraseña incorrecta</div>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          '</div>'
+        ].join('')
+        alertPlaceholder.append(wrapper);
+      }
+    }
+  });
+}
+
+
+
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,206 +1,132 @@
-var cuentas = [
-  { id:1,nombre: 'Mali', clave: 'MalimarAu', saldo: 200 },
-  { id:2,nombre: 'Gera', clave: 'GerardoOrtiz', saldo: 290 },
-  { id:3,nombre: 'Maui', clave: 'Mauiwi', saldo: 67 }
-];
-
-var jsonUsers = JSON.stringify(cuentas)
-localStorage.setItem('cuentas',jsonUsers  );
-
-
-const usuario = document.getElementById('usuario');
-const password = document.getElementById('password');
-const btnIniciar = document.getElementById('btnIniciar');
 const bienvenido = document.getElementById('bienvenido');
 const logo = document.getElementById('logo');
-const consulta = document.getElementById('consulta');
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+const movimientoSaldo = document.getElementById('movimientoSaldo');
+const consultarSaldo = document.getElementById('consultarSaldo');
+const ingresarSaldo = document.getElementById('ingresarSaldo');
+const retirarSaldo = document.getElementById('retirarSaldo');
+const btnConfirmar = document.getElementById('btnConfirmar');
+const alertInfo = document.getElementById('alertInfo');
+const numcantidad = document.getElementById('numcantidad');
 
-var usuarioCuenta = '';
-var userSelected = '';
+var tipoOperacion = '';
 
 logo.addEventListener('click', (e) => {
   window.location="../../index.html"; 
 });
 
-if(usuario)
-{
-  usuario.addEventListener('change',(e) => {
-      userSelected = usuario.selectedIndex;
-      let nombre = usuario.options[userSelected].text; 
-      bienvenido.innerHTML = `Bienvenido ${nombre}`;
-    });
-}
-
-if(btnIniciar){
-  btnIniciar.addEventListener('click', (e) => {
-    validarContra();
-  });
-}
-if(password){
-  password.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      validarContra();
-    }
-  });
-}
-
-if(consulta){
-  consulta.addEventListener('click', (e) => {
-    console.log(localStorage.getItem('nombre'));
+if(consultarSaldo){
+  consultarSaldo.addEventListener('click', (e) => {
     bienvenido.innerHTML = `Bienvenido ${localStorage.getItem('nombre')}`;
+    movimientoSaldo.style.opacity = 0;
+
+    let saldo = localStorage.getItem('saldo');
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-info alert-dismissible" role="alert">`,
+      `   <div>Tu saldo es de ${saldo} pesos. </div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+    alertInfo.append(wrapper);
+
   });
 }
 
-// pagina.addEventListener("load", function(event) {
-//   console.log(localStorage.getItem('nombre'));
-//   bienvenido.innerHTML = `Bienvenidosss ${localStorage.getItem('nombre')}`;
-// });
+if(ingresarSaldo){
+  ingresarSaldo.addEventListener('click', (e) => {
+    bienvenido.innerHTML = `Bienvenido ${localStorage.getItem('nombre')}`;
+    movimientoSaldo.style.opacity = 1;
+    tipoOperacion = 'I';
+    
+  });
+}
+
+if(retirarSaldo){
+  retirarSaldo.addEventListener('click', (e) => {
+    bienvenido.innerHTML = `Bienvenido ${localStorage.getItem('nombre')}`;
+    movimientoSaldo.style.opacity = 1;
+    tipoOperacion = 'R';
+  });
+}
+
+if(btnConfirmar){
+  btnConfirmar.addEventListener('click', (e) => {
+    e.preventDefault();
+    validarSaldo(tipoOperacion);
+  });
+}
 
 
-function validarContra(){
-  var allUsers = localStorage.getItem('cuentas');
-  var arrUsers =  JSON.parse(allUsers);
-  arrUsers.forEach(element => {
-    console.log("elemento: "+element.id);
-    if(element.id == userSelected){
-      if(password.value == element.clave){
-        usuarioCuenta = element.nombre;
-        console.log(usuarioCuenta);
-       localStorage.setItem('nombre',usuarioCuenta)
+function validarSaldo(){
+  let nuevoSaldo = 0;
+  var cuentas = localStorage.getItem('cuentas');
+  var arrCuentas =  JSON.parse(cuentas);
+  console.log(arrCuentas);
+  //debugger;
+  arrCuentas.forEach(element => {
+    if(element.id == localStorage.getItem('id')){
+     // debugger;
+      console.log(numcantidad.value);
+      if(tipoOperacion == 'I'){
+        nuevoSaldo = parseInt(element.saldo) + parseInt(numcantidad.value);
+        if(nuevoSaldo > 990){
+          const wrappererror = document.createElement('div')
+          wrappererror.innerHTML = [
+            `<div class="alert alert-danger alert-dismissible" role="alert">`,
+            `   <div>No se pueden ingresar $${numcantidad.value} porque da un total de $${nuevoSaldo} <br>
+                El saldo en la cuenta no debe ser mayor a $990</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+          ].join('')
+          alertInfo.append(wrappererror);
+        }
+        else{
+          element.saldo = nuevoSaldo;
+          console.log(arrCuentas);
+          //localStorage.setItem('cuentas',arrCuentas);
 
-        window.location="../../cajero.html"; 
-        
-        console.log(localStorage.getItem('nombre'));
-        bienvenido.innerHTML = `Bienvenidosss ${localStorage.getItem('nombre')}`;
-
-
+          const wrapper = document.createElement('div')
+          wrapper.innerHTML = [
+            `<div class="alert alert-success alert-dismissible" role="alert">`,
+            `   Se ingresaron correctamente $${numcantidad.value}, su nuevo saldo es de $${nuevoSaldo}`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+          ].join('')
+          alertInfo.append(wrapper);
+        } 
       }
       else{
-        const wrapper = document.createElement('div')
-        wrapper.innerHTML = [
-          `<div class="alert alert-danger alert-dismissible" role="alert">`,
-          `   <div>contraseña incorrecta</div>`,
-          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-          '</div>'
-        ].join('')
-        alertPlaceholder.append(wrapper);
+        nuevoSaldo = parseInt(element.saldo) - parseInt(numcantidad.value);
+        if(nuevoSaldo < 10){
+          const wrappererror = document.createElement('div')
+          wrappererror.innerHTML = [
+            `<div class="alert alert-danger alert-dismissible" role="alert">`,
+            `   <div>No se pueden retirar $${numcantidad.value} porque da un total de $${nuevoSaldo} <br>
+                El saldo en la cuenta no debe ser menor a $10</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+          ].join('')
+          alertInfo.append(wrappererror);
+        }
+        else{
+          element.saldo = nuevoSaldo;
+          console.log(arrCuentas);
+          //localStorage.setItem('cuentas',arrCuentas);
+
+          const wrapper = document.createElement('div')
+          wrapper.innerHTML = [
+            `<div class="alert alert-success alert-dismissible" role="alert">`,
+            `   Se retiraron correctamente $${numcantidad.value}, su nuevo saldo es de $${nuevoSaldo}`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+          ].join('')
+          alertInfo.append(wrapper);
+        } 
       }
+      
     }
   });
+  numcantidad.value ='';
 }
 
 
-
-
-
-
-// let carritoCompras = {}
-// let carritoTotal = 0
-// const total = document.getElementById('total')
-// const tableProducts = document.getElementById('tableProducts')
-// const form = document.getElementById('form')
-// const reset = document.getElementById('reset')
-
-// reset.addEventListener('click', (e) => {
-//     buildProductList(productList)
-// })
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault()
-//   //var existe = false;
-//   const searchInput = event.target.search.value.toUpperCase()
-//   const ProductListFilter = productList.filter((product) => {
-//     if (searchInput == product.nombre.toUpperCase()) {
-//       //existe = true;
-//       return product
-//     }
-//   })
-
-
-//   return buildProductList(ProductListFilter)
-
-// //   console.log(ProductListFilter)
-// })
-
-
-// search.addEventListener('keyup', (event) => {
-//   event.preventDefault()
-//   var existe = false;
-//   const searchInput = event.target.value.toUpperCase()
-
-//   const ProductListFilter = productList.filter((product) => {
-//     if (searchInput == product.nombre.toUpperCase().substring(0,event.target.value.length)) {
-//       existe = true;
-//       return product
-//     }
-//   })
-//   if(!existe){
-//     alert("El articulo "+ searchInput + " no existe");
-//   }
-//   return buildProductList(ProductListFilter);
-// })
-// const comprar = (nombre, precio, id) => {
-//   // console.log(nombre,precio, id)
-//   alert(
-//     `Hola, gracias por comprar el producto ${nombre} por un precio de $ ${precio}`,
-//   )
-
-//   // El metodo hasOwnProperty() devuelve un booleano indicando si un objeto tiene una propiedad especificada.
-//   if (carritoCompras.hasOwnProperty(id)) {
-//     carritoCompras[id].cantidad++
-//   } else {
-//     carritoCompras[id] = { nombre, precio, id }
-//     carritoCompras[id].cantidad = 1
-//   }
-
-//   carritoTotal += parseInt(carritoCompras[id].precio)
-//   total.innerHTML = `Total: $ ${carritoTotal}`
-
-//   tableProducts.innerHTML = ''
-
-//   for (const key in carritoCompras) {
-//     const childElement = document.createElement('tr')
-//     childElement.innerHTML = `
-//             <td>${carritoCompras[key].nombre}</td>
-//             <td>${carritoCompras[key].precio}</td>
-//             <td>${carritoCompras[key].cantidad}</td>
-//         `
-//     tableProducts.appendChild(childElement)
-//   }
-// }
-
-// const buildProductList = (productList1) => {
-//   //select the parent HTML tag
-//   var getParentElement = document.getElementById('productList')
-
-//   getParentElement.innerHTML = ''
-
-//   console.log(productList1)
-
-//   //Loop the product list array in order to generate the <section> </section>
-//   productList1.forEach((line) => {
-//     //creating the section cards
-//     const createSection = document.createElement('section')
-//     //adding a class
-//     createSection.classList.add('product') //<section class="product"> </section>
-//     //creating childs into the parent element section
-//     createSection.innerHTML = `
-//         <img src="${line.imagen}" alt="">
-//         <h2>${line.nombre}</h2>
-//         <h3>Price: <span>$ ${line.precio} </span> </h3>
-//         <p>Quantity: <span>${line.cantidad}</span> </p>
-//         <p>Description: <span>${line.descripcion}</span> </p>
-//         <p>Ml: <span>${line.ml} </span> </p>
-//         <button onclick="comprar('${line.nombre}', '${line.precio}', '${line.id}')">Buy </button>
-        
-//         `
-//     //
-//     getParentElement.appendChild(createSection)
-//   })
-// }
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   buildProductList(productList)
-// })
